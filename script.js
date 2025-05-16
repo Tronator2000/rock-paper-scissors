177571fb8c407ad18001880ce73dfa6d51e70d57
Tronator2000 <juanjo.morante.lopez@gmail.com>
2025-05-16 19:48:35 +0200

    Add playGame function and logic for 5 rounds

--- script.js
+++ script.js
@@ -13,34 +13,54 @@ function getHumanChoice(){
     return prompt("Choose between rock, paper or scissors");
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-    if (humanChoice == computerChoice){
-        console.log("Tie!");
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice == computerChoice){
+            console.log("Tie!");
+        }
+        else if (
+            humanChoice=="scissors" & computerChoice=="paper" | 
+            humanChoice=="paper" & computerChoice=="rock" |
+            humanChoice=="rock" & computerChoice=="scissors"
+        ) {
+            console.log(
+                "You win!, " +
+                humanChoice.charAt(0).toUpperCase().concat(humanChoice.slice(1)) +
+                " beats " +
+                computerChoice.charAt(0).toUpperCase().concat(computerChoice.slice(1))
+            );
+            humanScore++;
+        }
+        else {
+            console.log(
+                "You lose!, " +
+                computerChoice.charAt(0).toUpperCase().concat(computerChoice.slice(1)) +
+                " beats " +
+                humanChoice.charAt(0).toUpperCase().concat(humanChoice.slice(1))
+            );
+            computerScore++;
+        }
     }
-    else if (
-        humanChoice=="scissors" & computerChoice=="paper" | 
-        humanChoice=="paper" & computerChoice=="rock" |
-        humanChoice=="rock" & computerChoice=="scissors"
-    ) {
-        console.log(
-            "You win!, " +
-            humanChoice.charAt(0).toUpperCase().concat(humanChoice.slice(1)) +
-            " beats " +
-            computerChoice.charAt(0).toUpperCase().concat(computerChoice.slice(1))
-        );
-        humanScore++;
+
+    for (let i = 0; i < 5; i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    if (humanScore == computerScore){
+        console.log(`A tie!! ${humanScore} - ${computerScore}`)
+    }
+    else if (humanScore > computerScore) {
+        console.log(`You win!! ${humanScore} - ${computerScore}`);
     }
     else {
-        console.log(
-            "You lose!, " +
-            computerChoice.charAt(0).toUpperCase().concat(computerChoice.slice(1)) +
-            " beats " +
-            humanChoice.charAt(0).toUpperCase().concat(humanChoice.slice(1))
-        );
-        computerScore++;
+        console.log(`You lose!! :( ${humanScore} - ${computerScore}`);
     }
 }
+
+playGame();
